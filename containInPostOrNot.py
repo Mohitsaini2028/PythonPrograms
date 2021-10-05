72b133abfe47e960fb9374f90278df35f026afbe
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2021-10-05 15:26:37 +0530

    Add files via upload

---
+++ containInPostOrNot.py
@@ -0,0 +1,10 @@
+post = '''Simarjeet Sir is my Python Teacher.Teacher is a person with commendable skills for helping others learn, guiding, and helping solve a problem. Teachers should have excellent communication skills so that the students can understand everything and learn with ease. Teachers have several credentials as per their designation or where they are teaching. 
+
+Our parents also, to an extent, qualify as teachers, even though unprofessional, but they still work hard to guide us in our and pledge to help us from or even before when we are born. Teachers also grow with the experience they get while years of teaching in an institution. Teachers see many generations growing in front of them and, most times, become a significant factor in sculpting their personalities.
+
+'''
+
+if "Simarjeet" in post:
+    print("Post is talking about Simarjeet Sir")
+else:
+    print("Not talking ")
