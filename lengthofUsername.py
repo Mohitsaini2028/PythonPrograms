2685f6c6238ca3c737fc4d42ea273f6298ca64fb
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2021-10-09 16:20:20 +0530

    Add files via upload

---
+++ lengthofUsername.py
@@ -0,0 +1,10 @@
+# Write a program to check whether a username consists of less than 
+# 10 characters or not.
+
+username = input("Please Enter your Username : ")
+
+if len(username)<10:
+    print("Length of Username is less than 10 ")
+else:
+    print("Length of Username is more than 10 ")
+    
