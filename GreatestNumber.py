7adeb074744ec2a86d45cc2cae8ca176a9852932
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2021-10-09 16:07:08 +0530

    Add files via upload

---
+++ GreatestNumber.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter 1st Number : "))
+num2 = int(input("Enter 2nd Number : "))
+num3 = int(input("Enter 3rd Number : "))
+num4 = int(input("Enter 4rth Number : "))
+ 
+print(max(num1,num2,num3,num4))
