9ebb5affee143dc3289bdcff56d0dc8d356bbd54
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2021-10-30 11:43:52 +0530

    Add files via upload

---
+++ multiply.py
@@ -0,0 +1,4 @@
+#Accept two numbers from the user and multiply them together.
+f=int(input("Enter first number :"))
+s=int(input("Enter second number :"))
+print(f*s)
---
+++ valueofa.py
@@ -0,0 +1,4 @@
+#Python program that accepts an integer (a) and computes the value of a+aa+aaa.
+a=int(input("Enter the value of a : "))
+a=a+a*a+a*a*a
+print(a)
