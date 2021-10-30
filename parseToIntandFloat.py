25fd98ef18d22da685f0747b56fac6b93758013a
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2021-10-30 11:52:16 +0530

    Add files via upload

---
+++ parseToIntandFloat.py
@@ -0,0 +1,15 @@
+balance_str = "1500"
+balance_int = int(balance_str)
+
+
+# print the type
+print(type(balance_int))
+print(balance_int)
+
+balance_float = float(balance_str)
+
+# print the type
+print(type(balance_float))
+
+# print the value
+print(balance_float)
