153cd422d230be7f2f33d824160a324365efcf96
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2021-10-30 11:36:27 +0530

    Add files via upload

---
+++ product.py
@@ -0,0 +1,8 @@
+#takes two integer numbers and return their product.
+f=int(input("Enter first number :"))
+s=int(input("Enter second number :"))
+
+def product(a,b):
+    return a*b
+
+print(product(f,s))
