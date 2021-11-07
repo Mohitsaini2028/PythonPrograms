44ab1d1f8cb05a50a46c0dbb1e77b181601c2117
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2021-11-07 20:41:34 +0530

    Create upperAndLower.py

---
+++ upperAndLower.py
@@ -0,0 +1,5 @@
+#Write a Python program that takes input from the user and displays that input back in upper and lower 
+cases.
+user_input = input("What's your favourite language? ")
+print("My favourite language is ", user_input.upper())
+print("My favourite language is ", user_input.lower())
