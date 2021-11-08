56d985e1033c112e458bb73010e4d24d27d70d86
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2021-11-07 23:03:47 -0800

    Add files via upload

---
+++ maxAndMinThroughDone.py
@@ -0,0 +1,20 @@
+#Write the program that prompts the user for a list of numbers and prints out the maximum and minimum of the numbers at the end when the user enters “done”. Write the program to store the numbers the user enters in a list and use the max() and min() functions to compute the maximum and minimum numbers after the loop completes.
+
+my_list = []                        # Initialize array
+while True:
+    number = 0.0
+    input_number = input('Enter a number: ')
+    if input_number == 'done':
+        break
+
+    try:
+        number = float(input_number)
+    except ValueError:
+        print('Invalid input')
+        quit()
+
+    my_list.append(input_number)
+
+if my_list:
+    print('Maximum: ', max(my_list) or None)
+    print('Minimum: ', min(my_list) or None)
