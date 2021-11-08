dab27dae210d856369e34db9a72c1e9a972b8b7e
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2021-11-07 23:21:38 -0800

    Add files via upload

---
+++ greaterThanNumber.py
@@ -0,0 +1,5 @@
+#Write a Python program to find all the values in a list are greater than a given number.
+list1 = [220, 330, 500]
+list2 = [12, 17, 21]
+print(all(x >= 200 for x in list1))
+print(all(x >= 25 for x in list2))
